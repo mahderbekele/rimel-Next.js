7bb83800ccc6ff39b53eb3b7df6625242e39b226
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-08-16 23:35:42 +0300

    Added rimel next.js

--- rimel/src/app/page.tsx
+++ rimel/src/app/page.tsx
@@ -5,6 +5,8 @@ import Footer from "./components/footer";
 import Main from "./pages/home";
 import SignIn from "./pages/sign-in";
 import Signup from "./pages/sign-up";
+import ProductCarousel from "./pages";
+import HomePage from "./pages/category";
 
 export default function Home() {
   return (
@@ -12,9 +14,13 @@ export default function Home() {
 <Header />
 <Navbar />
 <Main />
+{/* <ProductCarousel /> */}
+<HomePage />
+
 {/* <Signup /> */}
 {/* <SignIn /> */}
       <Footer/>
+
      
     </div>
   );
